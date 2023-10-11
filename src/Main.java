dccefb5f7202d39b6e9b18af1dfef9113867f150
yeaokfr <144458128+yeaokfr@users.noreply.github.com>
2023-10-11 14:35:48 -0400

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        double celsius = 0.00;
+        double fahrenheit = 0.00;
+        boolean valid = false;
+
+        System.out.println("Please input your temperature in Celsius.");
+        do {
+            if (scan.hasNextDouble()) {
+                valid = true;
+            }
+            else{
+                scan.nextLine();
+                System.out.println("Invalid input, please try again");
+            }
+        } while (!valid) ;
+        celsius = scan.nextDouble();
+        fahrenheit = celsius * 9 / 5 + 32;
+
+        System.out.printf("%-10s%7.2f", "Degrees C:", celsius);
+        System.out.printf("\n%-10s%7.2f", "Degrees F", fahrenheit);
+
     }
 }
